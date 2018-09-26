3646b4604d536645d983f971f427b16d51c61376
Conlin Durbin <conlin@lessonly.com>
2018-09-26 12:59:30 -0400

    Adding sh file and new colors

---
+++ dank_neon.sh
@@ -0,0 +1,24 @@
+# Dank Neon
+# Jacoblin
+
+export background="#282A2D"
+export foreground="#F0F0F0"
+export selection_background="#FFFFFF"
+export selection_foreground="#33363A"
+
+export color0="#000000"
+#black
+export color1="#F92672"
+#red
+export color2="#74FA44"
+#green
+export color3="#FD971F"
+#yellow
+export color4="#66D9EF"
+#blue
+export color5="#AE81FF"
+#magenta
+export color6="#4ADBFF"
+#cyan
+export color7="#F0F0F0"
+#white
